d5199bbd454dc7bf8be7653dd53a75dc15cecc30
Артём Алексеевич Чувашов <shtrihtatt8@gmail.com>
2026-09-14 22:08:30 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,11 @@
-const CACHE='kw66-touch-recorder-v1';const ASSETS=['./','./index.html','./app.js','./manifest.json'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE = "kw66lab-v3-8";
+const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(hit => hit || fetch(e.request)));
+});
